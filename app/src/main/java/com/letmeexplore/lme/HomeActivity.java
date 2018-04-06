dec2e596cfdd62589396883d5b9f3780e72a1eae
Burak <ozyurtburak11@gmail.com>
2018-04-07 02:52:48 +0300

    Google Login Eklendi

---
+++ app/src/main/java/com/letmeexplore/lme/HomeActivity.java
@@ -0,0 +1,70 @@
+package com.letmeexplore.lme;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+    private HomeFragment homeFragment;
+    private BooksFragment booksFragment;
+    private SearchFragment searchFragment;
+    private ExploreFragment exploreFragment;
+    private FirebaseAuth mAuth;
+    private FirebaseUser firebaseUser;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMainNav=(BottomNavigationView)findViewById(R.id.main_nav);
+        mMainFrame=(FrameLayout)findViewById(R.id.main_frame);
+        homeFragment=new HomeFragment();
+        booksFragment=new BooksFragment();
+        searchFragment=new SearchFragment();
+        exploreFragment=new ExploreFragment();
+        ItemSelected();
+
+
+    }
+    void ItemSelected(){
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.nav_search:
+                        setFragment(searchFragment);
+                        return true;
+                    case R.id.nav_explore:
+                        setFragment(exploreFragment);
+                        return true;
+                    case R.id.nav_books:
+                        setFragment(booksFragment);
+                        return true;
+                    default:return false;
+                }
+            }
+        });
+
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction= getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
+
+    }
+}
--- app/src/main/java/com/letmeexplore/lme/MainActivity.java
+++ app/src/main/java/com/letmeexplore/lme/MainActivity.java
@@ -4,63 +4,144 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.animation.Animation;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView mMainNav;
-    private FrameLayout mMainFrame;
-    private HomeFragment homeFragment;
-    private BooksFragment booksFragment;
-    private SearchFragment searchFragment;
-    private ExploreFragment exploreFragment;
+    private ImageView ımageView;
+    private ImageView facebookLog;
+    private ImageView googleLog;
+    private ImageView signUp;
+    private ImageView logIn;
+    private EditText userName;
+    private EditText password;
+    private static final int RC_SIGN_IN=1;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private GoogleApiClient googleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mMainNav=(BottomNavigationView)findViewById(R.id.main_nav);
-        mMainFrame=(FrameLayout)findViewById(R.id.main_frame);
-        homeFragment=new HomeFragment();
-        booksFragment=new BooksFragment();
-        searchFragment=new SearchFragment();
-        exploreFragment=new ExploreFragment();
-        ItemSelected();
-
-
-    }
-    void ItemSelected(){
-        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        setContentView(R.layout.activity_login);
+        ımageView=(ImageView)findViewById(R.id.imageView);
+        facebookLog=(ImageView)findViewById(R.id.facebookAccount);
+        googleLog=(ImageView)findViewById(R.id.googleAccount);
+        signUp=(ImageView)findViewById(R.id.signUp);
+        logIn=(ImageView)findViewById(R.id.loginButton);
+        userName=(EditText)findViewById(R.id.userName);
+        password=(EditText)findViewById(R.id.password);
+        mAuth=FirebaseAuth.getInstance();
+        /*mAuthListener=new FirebaseAuth.AuthStateListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.nav_home:
-                        setFragment(homeFragment);
-                        return true;
-                    case R.id.nav_search:
-                        setFragment(searchFragment);
-                        return true;
-                    case R.id.nav_explore:
-                        setFragment(exploreFragment);
-                        return true;
-                    case R.id.nav_books:
-                        setFragment(booksFragment);
-                        return true;
-                        default:return false;
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()!=null){
+                    Intent ıntent=new Intent(MainActivity.this,HomeActivity.class);
+                    startActivity(ıntent);
                 }
             }
+        };*/
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //Login işlemi
+                Toast.makeText(getApplicationContext(),"Giris Yapıldı",Toast.LENGTH_SHORT).show();
+            }
         });
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleApiClient=new GoogleApiClient.Builder(getApplicationContext()).enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                Toast.makeText(MainActivity.this,"Bir Hata Var",Toast.LENGTH_SHORT).show();
+            }
+        }).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+        googleLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+                Toast.makeText(getApplicationContext(),"Google Giris Yapıldı",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
 
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    private void setFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction= getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.main_frame,fragment);
-        fragmentTransaction.commit();
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("TAG", "Google sign in failed", e);
+                // ...
+            }
+        }}
 
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                           // Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
     }
 
-}
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }}
