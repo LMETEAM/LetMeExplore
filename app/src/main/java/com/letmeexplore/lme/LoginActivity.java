ce350909105e3c3d202f5f4077a8490b23c5d224
Burak <ozyurtburak11@gmail.com>
2018-04-07 00:54:05 +0300

    User Login İnterface üzerinde çalışılıyor

--- app/src/main/java/com/letmeexplore/lme/LoginActivity.java
+++ app/src/main/java/com/letmeexplore/lme/LoginActivity.java
@@ -1,16 +1,18 @@
 package com.letmeexplore.lme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
     ImageView ımageView;
     ImageView facebookLog;
-    ImageView GoogleLog;
-    ImageView SıgnUp;
-    ImageView LogIn;
+    ImageView googleLog;
+    ImageView signUp;
+    ImageView logIn;
     EditText userName;
     EditText password;
 
@@ -19,5 +21,17 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ımageView=(ImageView)findViewById(R.id.imageView);
+        facebookLog=(ImageView)findViewById(R.id.facebookAccount);
+        googleLog=(ImageView)findViewById(R.id.googleAccount);
+        signUp=(ImageView)findViewById(R.id.signUp);
+        logIn=(ImageView)findViewById(R.id.loginButton);
+        userName=(EditText)findViewById(R.id.userName);
+        password=(EditText)findViewById(R.id.password);
+
+    }
+    void login(View view){
+        Intent ıntent=new Intent(this,MainActivity.class);
+        startActivity(ıntent);
+        finish();
     }
 }
--- app/src/main/java/com/letmeexplore/lme/MainActivity.java
+++ app/src/main/java/com/letmeexplore/lme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.letmeexplore.lme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
