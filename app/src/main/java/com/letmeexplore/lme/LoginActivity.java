56c930b0026b760fb093cace12513332905cb636
Burak <ozyurtburak11@gmail.com>
2018-04-07 00:28:40 +0300

    User Login İnterface üzerinde çalışılıyor

--- app/src/main/java/com/letmeexplore/lme/LoginActivity.java
+++ app/src/main/java/com/letmeexplore/lme/LoginActivity.java
@@ -2,10 +2,18 @@ package com.letmeexplore.lme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
     ImageView ımageView;
+    ImageView facebookLog;
+    ImageView GoogleLog;
+    ImageView SıgnUp;
+    ImageView LogIn;
+    EditText userName;
+    EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
