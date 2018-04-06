f06fc7b2b4ed8387b0f170a077e03e5532157e16
Burak <ozyurtburak11@gmail.com>
2018-04-06 17:12:19 +0300

    Initial commit

---
+++ app/src/main/java/com/letmeexplore/lme/MainActivity.java
@@ -0,0 +1,63 @@
+package com.letmeexplore.lme;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+    private HomeFragment homeFragment;
+    private BooksFragment booksFragment;
+    private SearchFragment searchFragment;
+    private ExploreFragment exploreFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMainNav=(BottomNavigationView)findViewById(R.id.main_nav);
+        mMainFrame=(FrameLayout)findViewById(R.id.main_frame);
+        homeFragment=new HomeFragment();
+        booksFragment=new BooksFragment();
+        searchFragment=new SearchFragment();
+        exploreFragment=new ExploreFragment();
+        ItemSelected();
+
+
+    }
+    void ItemSelected(){
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.nav_search:
+                        setFragment(searchFragment);
+                        return true;
+                    case R.id.nav_explore:
+                        setFragment(exploreFragment);
+                        return true;
+                    case R.id.nav_books:
+                        setFragment(booksFragment);
+                        return true;
+                        default:return false;
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction= getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
+
+    }
+
+}
