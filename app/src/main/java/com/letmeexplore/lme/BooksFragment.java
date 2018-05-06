1f3f6714f64ba382a073c0f35dddb407f742c4b4
ilker Atabay <ilkeratabay@yahoo.com>
2018-05-06 20:13:07 +0300

    Kullanıcı artık profil fotoğrafını dilediğince değiştirebiliyor.

--- app/src/main/java/com/letmeexplore/lme/BooksFragment.java
+++ app/src/main/java/com/letmeexplore/lme/BooksFragment.java
@@ -37,6 +37,7 @@ public class BooksFragment extends Fragment {
     public BooksFragment() {
         // Required empty public constructor
     }
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_books,container,false);
